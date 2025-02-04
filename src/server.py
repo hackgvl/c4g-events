61c521893ec943adf09b0a4df6684aebbf4476a3
oliviasculley <olivia@sculley.dev>
2025-02-04 14:37:04 -0500

    format with black

--- src/server.py
+++ src/server.py
@@ -115,7 +115,8 @@ async def slack_install():
     state = STATE_STORE.issue()
     url = AUTHORIZE_URL_GENERATOR.generate(state)
 
-    return Response(f"""
+    return Response(
+        f"""
         <a href="{html.escape(url)}">
         <img alt="Add to Slack" height="40" width="139"
              src="https://platform.slack-edge.com/img/add_to_slack.png"
@@ -123,7 +124,8 @@ async def slack_install():
                      https://platform.slack-edge.com/img/add_to_slack@2x.png 2x"
         />
         </a>
-    """)
+    """
+    )
 
 
 @API.get("/slack/auth")
